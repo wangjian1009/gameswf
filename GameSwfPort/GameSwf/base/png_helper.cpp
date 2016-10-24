92e5537df6fe9eefbac8188fc6fbeb7b7490fd6a
Loki <wangjian1009@gmail.com>
2016-10-24 11:14:09 +0800

    gameswf默认打开png支持

--- GameSwfPort/GameSwf/base/png_helper.cpp
+++ GameSwfPort/GameSwf/base/png_helper.cpp
@@ -74,7 +74,7 @@ namespace png_helper
 
 	void write_data(png_structp png_ptr, png_bytep data, png_size_t length)
 	{
-		fwrite(data, length, 1, (FILE*) png_ptr->io_ptr);
+		fwrite(data, length, 1, (FILE*) png_get_io_ptr(png_ptr));
 	}
 
 	void	write_rgba(FILE* out, uint8* data, int width, int height, int bpp)
